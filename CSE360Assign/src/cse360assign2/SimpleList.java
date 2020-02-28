419c42a5da89938c49c737ca683a168ce6cce5e9
mertayd <md2289@nau.edu>
2020-02-27 18:35:53 -0700

    Second version

--- CSE360Assign/src/cse360assign2/SimpleList.java
+++ CSE360Assign/src/cse360assign2/SimpleList.java
@@ -1,12 +1,15 @@
 package cse360assign2;
+
 /* @Author Mertay Dayanc
 * Class ID: 198
 * Assignment 2
 * Version: 2
 * This file contains the implementation of the class SimpleList.
 * SimpleList class, is a class that has basic properties such as adding elements to the first index and shift rest by 1.
-* It acts pretty much like stack, Last In First Out (LIFO) but SimpleList only has capability of holding maximum 10 
-* elements. User can get the actual number of elements and also display the SimpleList in string.
+* It acts pretty much like stack, Last In First Out (LIFO) but SimpleList has capability to increase it's size by 50% if needed
+* and also if 25% of the array is empty it decreases it's size 25% ensuring.
+* User can get the actual number of elements and also display the SimpleList in string as well as getting last element, first element and
+* learning size of the list and appending an element to the end.
 */
 
 
@@ -36,7 +39,7 @@ public class SimpleList {
 			count++;
 		}
 		else {
-			int new_size = (int)(count + count/2);
+			int new_size = (int)(count + Math.floor(count/2));
 			int[] temp = new int[new_size];
 			for(int i = 0; i < count; i++)
 			{
@@ -81,7 +84,7 @@ public class SimpleList {
 			double percent = count/list.length;
 			if(percent < 0.75)
 			{
-				int new_size = (int)(list.length - list.length/2);
+				int new_size = (int)(list.length - Math.floor(list.length/4));
 				int[] temp = new int[new_size];
 				for(int i = 0; i < count; i++)
 				{
@@ -120,7 +123,7 @@ public class SimpleList {
 	}
 	
 	/*
-	 * Returns the index of the given item, if not found returns -1 denotung not found
+	 * Returns the index of the given item, if not found returns -1 denoting not found
 	 * @param int item to be found
 	 * @return int item index in SimpleList
 	 */
@@ -137,4 +140,57 @@ public class SimpleList {
 		}
 		return index;
 	}
+	/*
+	 * Adds the int item at the end of the list
+	 * @param int item to be added
+	 * @return void
+	 */
+	void append(int item)
+	{
+		if(count == list.length)
+		{
+			int new_size = (int)(count + Math.floor(count/2));
+			int[] temp = new int[new_size];
+			for(int i = 0; i < count; i++)
+			{
+				temp[i] = list[i];
+			}
+			list = temp;
+		}
+		list[count] = item;
+		count++;
+	}
+	/*
+	 * Returns the value of the first item, if not found returns -1 denoting not found
+	 * @return int item value at 0th index in SimpleList
+	 */
+	int first()
+	{
+		if(count == 0)
+		{
+			return -1;
+		}
+		return list[0];
+	}
+	
+	/*
+	 * Returns the value of the last index, if not found returns -1 denoting not found
+	 * @return int last item value in SimpleList
+	 */
+	int last() {
+		if(count == 0)
+		{
+			return -1;
+		}
+		return list[count-1];
+	}
+	
+	/*
+	 * Returns the size of the list
+	 * @return int size of SimpleList
+	 */
+	int size() {
+		return list.length;
+	}
+	
 }
